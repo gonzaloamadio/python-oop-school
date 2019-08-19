243918b01366487b4bf8e86efe3f6851b3fb348c
gonzaloamadio <gonzaloamadio@gmail.com>
2019-08-20 00:57:15 +1000

    Tests for students enroling to courses and teachers teaching courses

--- tests/entities_test.py
+++ tests/entities_test.py
@@ -33,20 +33,48 @@ class TeacherFactory(factory.Factory):
 
 class StudentTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        self.student = StudentFactory()
+
     def test_student_basic_info(self):
-        student = StudentFactory()
+        student = self.student
         self.assertEqual(student.first_name, 'Gonzalo')
         self.assertEqual(student.last_name, 'Amadio')
         names = student.get_names()
         self.assertEqual(names, 'Gonzalo Amadio')
 
+    def test_student_has_courses(self):
+        student = self.student
+        courses = student.get_enroled_courses()
+        self.assertIsNotNone(courses)
+
+    def test_student_can_enrol_to_course_running(self):
+        student = self.student
+        course = CourseFactory()
+        res = student.enrol(course.get_code())
+        self.assertIsNotNone(res)
 
 class TeacherTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        self.teacher = TeacherFactory()
+
     def test_teacher_basic_info(self):
-        teacher = TeacherFactory()
+        teacher = self.teacher
         self.assertEqual(teacher.first_name, 'John')
         self.assertEqual(teacher.last_name, 'Doe')
         names = teacher.get_names()
         self.assertEqual(names, 'John Doe')
 
+    def test_teacher_has_courses(self):
+        teacher = self.teacher
+        courses = teacher.get_teaching_courses()
+        self.assertIsNotNone(courses)
+
+    def test_teacher_teaches_running_course(self):
+        teacher = self.teacher
+        course = CourseFactory()
+        res = teacher.add_course_to_teach(course.get_code())
+        self.assertIsNotNone(res)
